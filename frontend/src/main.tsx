dbada12d0ddc649902d3bdb46562feb3da55f453
Dylan Winter <72014821+DylanWTR@users.noreply.github.com>
2026-01-19 00:20:29 +0100

    feat(theme): prevent flashbang when changing something theme related

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import './styles/theme.css';
+import { bootstrapTheme } from './theme/bootstrapTheme';
+import { ThemeProvider } from './theme/ThemeProvider';
+
+bootstrapTheme();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
